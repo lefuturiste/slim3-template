84e922bee3b540b68b557141a2461ed973ed43e5
Le_Futuriste <spamfree@matthieubessat.fr>
2017-12-09 11:41:14 +0100

    Update App.php

--- App/App.php
+++ App/App.php
@@ -7,16 +7,22 @@ use Doctrine\Common\Cache\FilesystemCache;
 
 class App extends \DI\Bridge\Slim\App
 {
+	protected $bundles = [];
+	protected $hasBundles = false;
+	protected $twigPaths= ['../App/views'];
+
 	protected function configureContainer(ContainerBuilder $builder)
 	{
 		$builder->addDefinitions(__DIR__ . '/config/app.php');
 		$builder->addDefinitions(__DIR__ . '/config/api.php');
 		$builder->addDefinitions(__DIR__ . '/config/database.php');
 		$builder->addDefinitions(__DIR__ . '/config/containers.php');
-		if (getenv('PHP_DI_CACHE')) {
-			$builder->setDefinitionCache(new FilesystemCache('tmp/di'));
-			$builder->writeProxiesToFile(true, 'tmp/proxies');
-		}
+		$builder->addDefinitions(__DIR__ . '/config/bundles.php');
 	}
 
+	public function getBundles(){
+		return [
+			\App\AdminBundle\AdminBundle::class,
+		];
+	}
 }
